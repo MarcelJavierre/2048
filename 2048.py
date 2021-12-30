d0d7acdfac29da4bce8267b0b8da2ecc7ccbcec9
Marcel Javierre <javierremarcel@poli.ufrj.br>
2021-12-30 10:36:05 -0300

    Criado o arquivo principal do jogo Inserido a documentação sobre o jogo no início do arquivo

---
+++ 2048.py
@@ -0,0 +1,14 @@
+"""
+- Nome do jogo:
+    2048
+
+- Sobre o jogo:
+
+    2048 é jogado em um tabuleiro de 4X4, com peças numéricas que deslizam suavemente quando o jogador as move em um dos quatro sentidos disponíveis: para cima, para baixo, à esquerda e à direita.
+
+    Cada vez, um novo número aparece aleatoriamente em um local vazio na placa (com um valor de 2 ou 4).
+
+    Os blocos deslizam o mais longe possível na direção escolhida até que eles sejam interrompidos por qualquer outro bloco ou a borda do tabuleiro. Se duas peças do mesmo número colidem durante a movimentação, elas irão se fundir em uma peça com o valor total das duas peças que colidiram.
+
+    O jogo é vencido quando uma peça com o valor de 2048 aparece no tabuleiro. Quando o jogador não tem movimentos legais (não há espaços vazios e nem peças adjacentes com o mesmo valor), o jogo termina.
+"""
