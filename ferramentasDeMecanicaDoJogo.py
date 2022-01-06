f5649a870d2401b770c9fbfdd0010aa7b88fefc3
Marcel Javierre <javierremarcel@poli.ufrj.br>
2022-01-06 09:17:45 -0300

    Adicionado a super classe da mecânica do jogo

---
+++ ferramentasDeMecanicaDoJogo.py
@@ -0,0 +1,16 @@
+class ferramentasDeMecanicaDoJogo:
+    '''Super classe com métodos genéricos associados com a mecânica do jogo'''
+
+    def geraMatriz(self, numeroDeLinhas, numeroDeColunas):
+        '''
+        Método para gerar uma matriz. Retorna uma lista de listas (matriz).
+        self,int,int -> list
+        '''
+        return None
+
+    def geraNumeroAleatorio(self, listaDeNumeros):
+        '''
+        Método para sortear um número aleatório de uma dada lista. Retorna o número sorteado.
+        self,list -> int
+        '''
+        return None
--- mecanicaDoJogo.py
+++ mecanicaDoJogo.py
@@ -1,4 +1,28 @@
+# Importando a super classe com os métodos genéricos associados com a mecânica do jogo
+import ferramentasDeMecanicaDoJogo
+
 # Classe da seção mecânica do jogo
-class mecanicaDoJogo:
-    '''Esta classe é responsável por alterar a tela e o estado do jogo de acordo com os comandos do jogador'''
-    pass
+class mecanicaDoJogo(ferramentasDeMecanicaDoJogo):
+    '''Esta classe contém todos os métodos responsável por alterar a tela e o estado do jogo de acordo com os comandos do jogador'''
+
+    def __init__(self, tamanhoDoTabuleiro = 4):
+        '''
+        Inicializa o tabuleiro. Se nenhum parâmetro for passado, inicializa com o tamanho padrão 4X4.
+        self,int -> none
+        '''
+        self.tabuleiro = ferramentasDeMecanicaDoJogo.geraMatriz(self, tamanhoDoTabuleiro, tamanhoDoTabuleiro)
+        self.score = 0
+
+    def getTabuleiro(self):
+        '''
+        Método que retorna a matriz do tabuleiro.
+        self -> list
+        '''
+        return self.tabuleiro
+
+    def getScore(self):
+        '''
+        Método que retorna o score.
+        self -> int
+        '''
+        return self.score
