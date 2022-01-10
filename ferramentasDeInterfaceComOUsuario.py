300331af3b1202dd7a291677d24ce45f0b892009
Marcel Javierre <javierremarcel@poli.ufrj.br>
2022-01-10 08:11:37 -0300

    Atualizado o método limpaTela

--- ferramentasDeInterfaceComOUsuario.py
+++ ferramentasDeInterfaceComOUsuario.py
@@ -1,8 +1,10 @@
-# Biblioteca para utilizar comandos do terminal
-import os
+# Módulo da biblioteca sys para verificar qual sistema operacional está executando o código
+from sys import platform
+# Módulo da biblioteca os para utilizar comandos do terminal
+from os import system
 
 class ferramentasDeInterfaceComOUsuario:
-    '''Super classe com os métodos genéricos associados com a comunicação com o usuário'''
+    '''Super classe com os métodos genéricos associados com a comunicação com o usuário.'''
 
     def entradaDoUsuario(self):
         '''
@@ -16,4 +18,16 @@ class ferramentasDeInterfaceComOUsuario:
         Método para limpar a tela do terminal.
         self -> none
         '''
-        pass
+
+        # Verifica qual é o sistema operacional executando o código
+        # Se for windows, utiliza o comando cls para limpar a tela
+        if platform == 'win32':
+            system('cls')
+
+        # Se for linux, utiliza o comando clear
+        elif platform == 'linux':
+            system('clear')
+
+        # Se não for nenhum dos dois, não faz nada
+        else:
+            pass
