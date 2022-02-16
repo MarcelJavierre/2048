03b254cf9cb937c2b84676566b1a29b87bd59107
Marcel Javierre <javierremarcel@poli.ufrj.br>
2022-02-16 08:09:59 -0300

    Adicionado tratamento de exceção ErroDeEstatistica

--- 2048.py
+++ 2048.py
@@ -714,8 +714,14 @@ def estatisticas():
     # Limpa a tela
     interface.limpaTela()
 
-    # Mostra a tela das estatísticas
-    interface.telaDasEstatisticas(log.getEstatisticasDeJogadas(), log.getEstatisticasDePecas(), log.getEstatisticasDeScore())
+    try:
+        # Mostra a tela das estatísticas
+        interface.telaDasEstatisticas(log.getEstatisticasDeJogadas(), log.getEstatisticasDePecas(), log.getEstatisticasDeScore())
+
+    # Caso não possua estatísticas, gera um relatório de erro e retorna ao menu principal
+    except ErroDeEstatistica as mensagemDeErro:
+        # Gera o relatório de erro
+        log.relatorioDeErro(repr(mensagemDeErro))
 
     # Volta para o menu principal
     main()
